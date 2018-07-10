7414b01369cfcba3bb588da64358071df94b1113
Cyrus Fox <cyrus@lambdacore.de>
2018-07-10 11:30:07 +0200

    Updated gluon-autorestart to work with Gluon 2018.1

--- gluon-autorestart/check_site.lua
+++ gluon-autorestart/check_site.lua
@@ -1,3 +1,3 @@
-need_string 'autorestart.interval'
-need_number 'autorestart.starthour'
-need_number 'autorestart.endhour'
+need_string {'autorestart', 'interval'}
+need_number {'autorestart', 'starthour'}
+need_number {'autorestart', 'endhour'}
